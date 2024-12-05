e73035c9b3a108251693e66cd3872fefd8e34df3
Bhavik Chand <hyruka98@gmail.com>
2024-12-05 11:52:37 -0800

    Lessons page is in progress, no styling yet. Needs to be linked to lessonsDetails/all flashcards from the user.

---
+++ quizWhiz/app/lessonsDetails.jsx
@@ -0,0 +1,12 @@
+import PublicLessons from "../components/LessonsDetails/lessonsDetails";
+import { View } from "react-native";
+
+const Page = () => {
+  return (
+    <View>
+      <PublicLessons />
+    </View>
+  );
+}
+
+export default Page;
