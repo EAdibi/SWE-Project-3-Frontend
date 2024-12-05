b8c753872bcb3cada01590c8b0a14053e7530b73
Sumit <sumitsunchu@gmail.com>
2024-12-05 11:26:23 -0800

    Integrated Admin Page and Settings Page

--- quizWhiz/app/homepage.jsx
+++ quizWhiz/app/homepage.jsx
@@ -1,10 +1,22 @@
-import { View, StyleSheet } from 'react-native';
+import { View, ScrollView, StyleSheet } from 'react-native';
 import HomeScreen from '../components/Homepage/homepage';
 
 export default function Page() {
   return (
-    <View>
+    <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
       <HomeScreen />
-    </View>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: '100vh', 
+    backgroundColor: 'transparent', 
+  },
+  contentContainer: {
+    flexGrow: 1,  
+    minHeight: '100%',
+  },
+});
